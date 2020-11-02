2e8d8daac19dbc42a2f1ff9b44c2a545c2221b70
dededeebebebe <htdmtnr723a@gmail.com>
2020-11-03 06:53:11 +0900

    テンプレートタグ挿入／テンプレートの分割

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
     <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Agency</title>
-        <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet" />
+        <?php get_header(); ?>
     </head>
-    <body id="page-top">
+    <body id="page-top" <?php body_class(); ?>>
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div class="container">
@@ -534,15 +521,6 @@
                 </div>
             </div>
         </div>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
-        <!-- Third party plugin JS-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-        <!-- Contact form JS-->
-        <script src="<?php echo get_template_directory_uri(); ?>/assets/mail/jqBootstrapValidation.js"></script>
-        <script src="<?php echo get_template_directory_uri(); ?>/assets/mail/contact_me.js"></script>
-        <!-- Core theme JS-->
-        <script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
+        <?php get_footer(); ?>  
     </body>
 </html>
